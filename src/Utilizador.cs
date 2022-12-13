ee22f9952aff6f0857891d1d504c4fa4b6cde25b
EduardoHenriques <61889790+EduardoHenriques@users.noreply.github.com>
2022-12-13 14:33:58 +0000

    Add files via upload

---
+++ src/Utilizador.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FeirasEspinho
+{
+    [Serializable]
+    internal class Utilizador
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
