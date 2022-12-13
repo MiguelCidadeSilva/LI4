39b8d8323744ae93693af289e93d0aabe46be3ff
Eduardo Henriques <fernando28henriques@gmail.com>
2022-12-13 15:16:18 +0000

    user inicial

--- src/Utilizador.cs
+++ src/Utilizador.cs
@@ -1,5 +1,5 @@
 ﻿
-1
+
 using System.ComponentModel.DataAnnotations;
 
 namespace FeirasEspinho
@@ -7,7 +7,31 @@ namespace FeirasEspinho
     [Serializable]
     internal class Utilizador
     {
-        static void Main(string[] args)
+        private String nome;
+        private String password;
+        private String email;
+
+
+        public Utilizador()
+        {
+            this.nome = "";
+            this.password = "";
+            this.email = "";
+        }
+
+        public Utilizador(String nome, String password, String email)
+        {
+            this.nome = nome;
+            this.password = password;
+            this.email = email;
+        } 
+        
+    
+    }
+
+    class Teste
+    {
+        static void Main(String[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("yes");
