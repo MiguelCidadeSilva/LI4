c5f011367af8bec3e598f2911afd4c864ab766a3
José Carvalho <zebragapt@gmail.com>
2022-12-16 13:11:19 +0000

    Continued working on web pages

---
+++ FeirasEspinhoBlazorApp/FeirasEspinhoBlazorApp/SourceCode/WebClasses/NavBar.cs
@@ -0,0 +1,67 @@
+﻿using static FeirasEspinhoBlazorApp.Shared.NavMenu;
+
+namespace FeirasEspinhoBlazorApp.SourceCode.WebClasses
+{
+    public class NavBar
+    {
+        private Dictionary<int, List<Opcao>> menus = new Dictionary<int, List<Opcao>>();
+        public enum menusNomes { Cliente, Admin, Feirante, Login, Geral };
+        private void InicializeLoginNav()
+        {
+            int lo = (int)menusNomes.Login;
+            this.menus[lo] = new List<Opcao>
+            {
+                new Opcao("Início", "oi oi-home", ""),
+                new Opcao("Login", "oi oi-account-login", "login"),
+                new Opcao("Criar Conta", "oi oi-person", "createacc")
+            };
+        }
+        private void InicializeClientNav()
+        {
+            int cl = (int)menusNomes.Cliente;
+            this.menus[cl] = new List<Opcao>
+            {
+                new Opcao("Consultar Feiras", "oi oi-dashboard", ""),
+                new Opcao("Leilões", "oi oi-euro", ""),
+                new Opcao("Negociações", "oi oi-transfer", "")
+            };
+        }
+
+        private void InicializeFeiranteNav()
+        {
+            int fe = (int)menusNomes.Feirante;
+            this.menus[fe] = new List<Opcao>();
+        }
+        private void InicializeAdminNav()
+        {
+            int ad = (int)menusNomes.Admin;
+            this.menus[ad] = new List<Opcao>();
+        }
+        private void InicializeGeralNav()
+        {
+            int cl = (int)menusNomes.Cliente;
+            int ad = (int)menusNomes.Admin;
+            int fe = (int)menusNomes.Feirante;
+            int ge = (int)menusNomes.Geral;
+            this.menus[ge] = new List<Opcao>();
+            this.menus[ge].ForEach(op =>
+            {
+                this.menus[cl].Add(op);
+                this.menus[ad].Add(op);
+                this.menus[fe].Add(op);
+            });
+        }
+        public List<Opcao> getMenu(int menuApresentar)
+        {
+            return this.menus[menuApresentar];
+        }
+        public NavBar()
+        {
+            this.InicializeLoginNav();
+            this.InicializeClientNav();
+            this.InicializeFeiranteNav();
+            this.InicializeAdminNav();
+            this.InicializeGeralNav();
+        }
+    }
+}
---
+++ FeirasEspinhoBlazorApp/FeirasEspinhoBlazorApp/SourceCode/WebClasses/Opcao.cs
@@ -0,0 +1,29 @@
+﻿namespace FeirasEspinhoBlazorApp.SourceCode.WebClasses
+{
+    public class Opcao
+    {
+        private string texto;
+        private string simbolo;
+        private string link;
+        public Opcao(string texto, string simbolo, string link)
+        {
+            this.texto = texto;
+            this.simbolo = simbolo;
+            this.link = link;
+        }
+        public string Texto
+        {
+            get => texto; 
+        }
+
+        public string Simbolo
+        {
+            get => simbolo;
+        }
+
+        public string Link
+        {
+            get => link;
+        }
+    }
+}
