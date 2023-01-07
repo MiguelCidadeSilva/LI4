24b2299fe49ad649f1ec9386036dacda4327d71c
Miguel Silva <mikefsi@hotmail.com>
2023-01-07 18:52:53 +0000

    Forgot to add this file

---
+++ FeirasEspinhoBlazorApp/FeirasEspinhoBlazorApp/SourceCode/Feiras/Candidatura.cs
@@ -0,0 +1,57 @@
+﻿using FeirasEspinhoBlazorApp.SourceCode.Stands;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FeirasEspinhoBlazorApp.SourceCode.Feiras
+{
+    public class Candidatura
+    {
+        private int idCandidatura;
+        private DateTime dataSubmissao;
+        private bool aprovacao;
+        private int idStand;
+        private int idFeira;
+
+        public int IdCandidatura
+        {
+            get { return idCandidatura; }
+            set { idCandidatura = value; }
+        }
+
+        public DateTime DataSubmissao
+        {
+            get { return dataSubmissao;}
+            set { dataSubmissao = value;}
+        }
+
+        public bool Aprovacao
+        {
+            get { return aprovacao; }
+            set { aprovacao = value; }
+        }
+
+        public int IdStand
+        {
+            get { return idStand; }
+            set { idStand = value; }
+        }
+
+        public int IdFeira
+        {
+            get { return idFeira;}
+            set {idFeira = value;}
+        }
+
+        public Candidatura(int idCandidatura, DateTime dataSubmissao, bool aprovacao, int idStand, int idFeira)
+        {
+            IdCandidatura = idCandidatura;
+            DataSubmissao= dataSubmissao;
+            Aprovacao = aprovacao;
+            IdStand = idStand;
+            IdFeira = idFeira;
+        }
+    }
+}
